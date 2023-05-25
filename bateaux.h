04943ebd522cb4fe6c36cb40f7486c43243a432d
sara <sara.camassa@heig-vd.ch>
2023-05-25 16:17:00 +0200

    temporary structure

--- bateaux.h
+++ bateaux.h
@@ -1,4 +1,38 @@
 #ifndef LABO3_BATEAUX_BATEAUX_H
 #define LABO3_BATEAUX_BATEAUX_H
 
+#include <stdint.h>
+
+typedef char *Nom;
+//typedef enum {VOILIER, PECHE, PLAISANCE} type;
+
+struct Plaisance{
+	uint8_t longueurBateau;
+	Nom nomProprietaire;
+};
+
+union PecheOuPlaisance {
+	uint8_t tonnePoisson;
+	struct Plaisance plaisance;
+};
+
+struct Moteur{
+	uint16_t puissanceMoteur;
+	union PecheOuPlaisance pecheOuPlaisance;
+};
+
+union VoilierOuMoteur {
+	uint16_t surfaceVoilure;
+	struct Moteur moteur;
+};
+
+struct bateau {
+	Nom nom;
+	union VoilierOuMoteur voilierOuMoteur;
+};
+
+struct bateau B1 = {.nom = "test", .voilierOuMoteur.moteur.puissanceMoteur = 2,
+	.voilierOuMoteur.moteur.pecheOuPlaisance.tonnePoisson = 5}; //c'est trop moche
+	// on va changer
+
 #endif //LABO3_BATEAUX_BATEAUX_H
