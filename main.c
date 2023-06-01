15575ed51f5ce1bd6c9dd3a20c07a6a86a3a9da1
sara <sara.camassa@heig-vd.ch>
2023-06-01 14:43:06 +0200

    fonction mediane ecart type

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "bateaux.h"
 #include "bateaux.c"
 
@@ -9,6 +10,12 @@ int comparerTaxes(const Bateau* el1, const Bateau el2){
 	int t2 = el2.taxeAnuelle.taxeAnnuelle;
 	if (t1 < t2) return 1;
 	if (t1 > t2) return -1; //inversés pour être décroissant
+	return 0;
+};
+
+int comparerDouble(const double el1, const double el2){
+	if (el1 < el2) return -1;
+	if (el1 > el2) return 1;
 	return 0;
 };
 
@@ -45,16 +52,38 @@ void afficherBateau(Bateau b){
 }
 
 void afficherParType(Bateau port[], int size, Categorie cat, SousCategorie sousCat){
-	int compteurBateaux = 0, somme = 0, moyenne = 0, mediane = 0, ecartType = 0;
+	int compteurBateaux = 0;
+	double somme = 0, moyenne = 0, mediane = 0, ecartType = 0;
+	double taxes[10];
 	for(int i = 0; i < size; ++i){
 		if(port[i].categorie == cat && port[i].specificites.moteur.sousCategorie ==
 			sousCat){
+			taxes[compteurBateaux] = port[i].taxeAnuelle.taxeAnnuelle;
+			//somme
 			somme += port[i].taxeAnuelle.taxeAnnuelle;
 			compteurBateaux++;
 		}
 	}
+
+	//moyenne
 	moyenne = somme/compteurBateaux;
-	printf("somme: %d - moyenne: %d\n", somme, moyenne);
+
+	int taille = sizeof(taxes)/sizeof(double);
+
+	//ecart-type
+	for (int i = 0; i < taille; ++i) {
+		ecartType += pow(taxes[i] - moyenne, 2.0);
+	}
+	ecartType = ecartType / (taille - 1);
+	ecartType = sqrt(ecartType/taille);
+
+	//mediane
+	qsort(taxes, taille, sizeof(double), comparerDouble);
+	mediane = (taille % 2) == 0 ? taxes[taille/2] : (taxes[taille/2 - 1] +
+		taxes[taille/2])/2;
+
+	printf("somme: %.1f - moyenne: %.1f - mediane: %.1f - ecart-type: %f\n", somme,
+			 moyenne, mediane, ecartType);
 }
 
 int main() {
@@ -76,7 +105,12 @@ int main() {
 			.specificites.moteur.sousCategorie = PLAISANCE,
 			.specificites.moteur.puissance = 90,
 			.specificites.moteur.sousSpecificites.plaisance.longueur = 70,
-			.specificites.moteur.sousSpecificites.plaisance.nomProprietaire = "Mr.Cargo"}
+			.specificites.moteur.sousSpecificites.plaisance.nomProprietaire = "Mr"
+																									".Cargo"},
+		{.nom = "peche2", .categorie = MOTEUR,
+			.specificites.moteur.sousCategorie = PECHE,
+			.specificites.moteur.puissance = 500,
+			.specificites.moteur.sousSpecificites.peche.tonnePoisson = 350},
 	};
 
 	//calculer toutes les taxes annuelles
